500cdc9fe7cabbcf35f9fc75eb4fc5bb5d9f5772
Lukman <61648308+Lukman350@users.noreply.github.com>
2021-01-14 12:31:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Title from './Title';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title />
-      </header>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
 
